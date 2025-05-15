4f893fb03d8ff5a73609db8f3d65de965eb26757
Najmuddin Khalifaev <khalifaevnajmuddin1@gmail.com>
2025-05-15 18:06:03 -0500

    Testing the latest change on the pipeline which is orders page

--- tests/demo/orders.spec.js
+++ tests/demo/orders.spec.js
@@ -4,15 +4,13 @@ import { orders } from '../pages/orders';
 
 test.describe('Web Orders Page Title Verification', () => {
   test.beforeEach(async ({ page }) => {
-    // Navigate to the login page
+    // Navigate to the Web Orders page
     await page.goto('');
-    
-    // Login with valid credentials
+
     await page.locator(login.username).fill('Tester');
     await page.locator(login.password).fill('test');
     await page.locator(login.submitButton).click();
-    
-    // Verify successful login
+  
     await expect(page).toHaveURL('http://secure.smartbearsoftware.com/samples/testcomplete12/weborders/');
   });
 
@@ -20,17 +18,15 @@ test.describe('Web Orders Page Title Verification', () => {
     // Navigate to View all orders page
     await page.locator(orders.viewAllOrdersLink).click();
     
-    // Verify the page title
+    //Verify the Title of the page
     const pageTitle = await page.title();
     expect(pageTitle).toBe('Web Orders');
     console.log(`Verified page title: ${pageTitle}`);
     
-    // Additional verification: Check the heading on the page
+    // Additional verification: to check the header of the page
     const heading = await page.locator('h2').textContent();
     expect(heading).toContain('List of All Orders');
     console.log(`Verified page heading: ${heading}`);
   });
-
-  //test more scenarios 
   
 });
